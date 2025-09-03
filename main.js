78667dcfc61982311d4166f466ee882318bad7e7
Rohit Rajaram Kolekarr <rohitrajaram.kolekar@gmail.com>
2025-09-03 22:58:11 +0100

    Initial commit.

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-text', {
+    strings: ['Mechanical Engineer', 'Certified SolidWorks Professional','Embedded Systems Developer', 'Sustainable Innovator','Automotive Enthusiast'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
